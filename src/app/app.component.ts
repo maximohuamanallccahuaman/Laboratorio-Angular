e8c8c6f4ad0d05035fa94d864ab2c91486ca267d
Maximo Huaman Allccahuaman <mrenato.huaman@vallegrande.edu.pe>
2021-11-15 14:53:17 -0500

    Laboratorio Angular avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub;
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000)
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+  
+  
   title = 'ANGULAR-VIDEOS-SERIES';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
