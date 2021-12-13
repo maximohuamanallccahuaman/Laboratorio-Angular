a37e7c0d9a4ab887251b72837d81797b71e8dde1
Maximo Huaman Allccahuaman <mrenato.huaman@vallegrande.edu.pe>
2021-12-13 16:29:16 -0500

    Aplicación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component';
+import { ListaComponent } from './dashboard/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent,
+    TablaComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
